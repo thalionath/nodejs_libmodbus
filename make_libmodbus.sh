81f73b2a700ece8494ffa728ad716e93ba25005a
CDA0 <cdazero@gmail.com>
2013-02-08 17:06:44 +0000

    updated to current libmodbus git

--- make_libmodbus.sh
+++ make_libmodbus.sh
@@ -5,9 +5,10 @@ git clone https://github.com/stephane/libmodbus.git
 cd ./libmodbus/
 
 # reset to patch commit number
-git checkout 79a7c1ea82e5bb2e13c8efc77cb3e8a6d7368c58
+git checkout 60adc0506666d19f9d8bd37bde8d8a728124047f
 
-patch -c -p2 < ../mt.patch
+#patch -c -p2 < ../mt.patch
+git apply ../mt.patch
 
 #./autogen.sh && ./configure --enable-static=yes --enable-shared=no && make
 ./autogen.sh && ./configure && make
