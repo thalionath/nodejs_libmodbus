137cee02728cd4e0117d87a20f891ebb77f44314
thalionath <thalionath@gmail.com>
2013-02-12 17:08:43 +0100

    added patched libmodbus https://github.com/stephane/libmodbus.git 60adc0506666d19f9d8bd37bde8d8a728124047f

---
+++ deps/libmodbus/autogen.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+autoreconf --install --symlink --force
+
+
+echo
+echo "------------------------------------------------------"
+echo "Initialized build system. You can now run ./configure "
+echo "------------------------------------------------------"
+echo
